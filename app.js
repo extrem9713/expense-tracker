8fef059d3b50382856cac52f955b6fbdd5d242bb
extrem9713 <extrem9713@gmail.com>
2021-07-20 19:05:12 +0800

    feat: fix handlebarsHelper

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const toDate = require('./tools/handlebarsHelpers')
 const routes = require('./routes')
 const app = express()
 const PORT = process.env.PORT || 3000
 
 require('./config/mongoose')
 
-app.engine('hbs', exphbs({defaultLayout:'main', extname:'.hbs'}))
+app.engine('hbs', exphbs({defaultLayout:'main',
+ extname:'.hbs', helpers: { toDate }}))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
--- tools/handlebarsHelpers.js
+++ tools/handlebarsHelpers.js
@@ -3,3 +3,5 @@ function toDate(date) {
   .toISOString()
   .split("T")[0]
 }
+
+module.exports = toDate
