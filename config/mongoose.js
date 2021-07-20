292a12adab60b2e2eb95a6f9a233f36cb9666fad
extrem9713 <extrem9713@gmail.com>
2021-07-20 15:29:36 +0800

    set mongodb

---
+++ config/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const db = mongoose.connection
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/expence-tracker'
+
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected !')
+})
+
+module.exports = db
