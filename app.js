41a418e751ca9333af7394357fc3a2af00272576
extrem9713 <extrem9713@gmail.com>
2021-07-20 15:35:09 +0800

    install express-handlebars

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const PORT = process.env.PORT || 3000
 
 require('./config/mongoose')
 
+app.engine('hbs', exphbs({defaultLayout:'main', extname:'.hbs'}))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({extended: true}))
