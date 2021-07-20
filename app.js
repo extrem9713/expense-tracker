2a750ed597abd5dbbfe81105a5080f7ad3eaa5d1
extrem9713 <extrem9713@gmail.com>
2021-07-20 15:39:45 +0800

    install method-override

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const app = express()
 const PORT = process.env.PORT || 3000
 
@@ -8,3 +9,8 @@ require('./config/mongoose')
 app.engine('hbs', exphbs({defaultLayout:'main', extname:'.hbs'}))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+
+app.listen(PORT, () => {
+  console.log(`this app is running on http://localhost:${PORT}`)
+})
