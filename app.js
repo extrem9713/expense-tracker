27f5c24ec340889a4a519d00a89e833bb5ca50dd
extrem9713 <extrem9713@gmail.com>
2021-07-20 16:57:15 +0800

    feat: add home.js

--- app.js
+++ app.js
@@ -10,10 +10,7 @@ app.engine('hbs', exphbs({defaultLayout:'main', extname:'.hbs'}))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
-
-app.get('/',(req, res) => {
-  res.render('index')
-})
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`this app is running on http://localhost:${PORT}`)
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,1 +1,27 @@
 const express = require('express')
+const Category = require('../../models/category')
+const record = require('../../models/record')
+const Record = require('../../models/record')
+const router = express.Router()
+
+router.get('/', (req,res) =>{
+  const category = req.query.category
+  const filter = {}
+  if(category){filter.category = category}
+
+  const categories = []
+  Category.find()
+  .lean()
+  .then(category => categories.push(...category))
+  .catch(error => console.error(error))
+
+  Record.find(filter)
+  .populate('category')
+  .lean()
+  .then((records) => {
+    let totalAmount = 0
+    records.forEach(record => totalAmount += record.amount)
+    res.render('index',{categories, category, record, totalAmount})
+  })
+  .catch(error => console.error(error))
+})
