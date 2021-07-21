e186ff79b5415e218c39b9af5221ddc221e9aa4b
extrem9713 <extrem9713@gmail.com>
2021-07-21 12:43:05 +0800

    feat: add edit function

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -13,9 +13,31 @@ Category.find()
 router.get('/new', (req, res)=>{
   res.render('new', { categories })
 })
+
 router.post('/',(req, res) =>{
   Record.create(req.body)
     .then(() => res.redirect('/'))
+    .catch(error => console.error(error))
+})
+
+//edit
+router.get('/:id/edit',(req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .lean()
+    .then(record => res.render('edit', {categories, record}))
+    .catch(error => console.error(error))
+})
+
+router.put('/:id',(req, res) => {
+  const id = req.params.id  
+  return Record.findById(id)
+    .then(record => {
+      //用來複製一個或多個物件自身所有可數的屬性到另一個目標物件。
+      Object.assign(record, req.body)
+      return record.save()
+  })
+    .then(() =>res.redirect('/'))
     .catch(error => console.error(error))
 })
 
