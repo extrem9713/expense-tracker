ab48411e0d37d99119afcbd212afbde66354176b
extrem9713 <extrem9713@gmail.com>
2021-07-21 12:48:46 +0800

    add annotation for handlebarsHelpers

--- tools/handlebarsHelpers.js
+++ tools/handlebarsHelpers.js
@@ -1,5 +1,7 @@
 function toDate(date) {
+  //可返回格林威治時間和本地時間之間的時差，以分鐘為單位
   return new Date(date.getTime() - (date.getTimezoneOffset()*60*1000))
+  //返回YYYY-MM-DDTHH
   .toISOString()
   .split("T")[0]
 }
